ca613188e22a81a05eac0220fd6b93ec4e3e1200
morganp5 <11314976@mail.dcu.ie>
2016-02-21 15:36:55 +0000

    Added Clojurescript and Reagent to guestbook application

--- guestbook/src/clj/guestbook/routes/home.clj
+++ guestbook/src/clj/guestbook/routes/home.clj
@@ -1,37 +1,36 @@
 (ns guestbook.routes.home
-  (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET POST]]
-            [ring.util.http-response :as response]
-            [guestbook.db.core :as db]
-            [bouncer.core :as b]
-            [bouncer.validators :as v]))
+    (:require
+      [guestbook.layout :as layout]
+      [compojure.core :refer [defroutes GET POST]]
+      [ring.util.http-response :as response]
+      [guestbook.db.core :as db]
+      [bouncer.core :as b]
+      [bouncer.validators :as v]
+      [ring.util.response :refer [response status]]))
 
-(defn home-page [{:keys [flash]}]
-  (layout/render
-    "home.html"
-    (merge {:messages (db/get-messages)}
-           (select-keys flash [:name :message :errors]))))
+(defn home-page []
+      (layout/render "home.html"))
 
 (defn validate-message [params]
-  (first
-    (b/validate
-      params
-      :name v/required
-      :message [v/required [v/min-count 10]])))
+      (first
+        (b/validate
+          params
+          :name v/required
+          :message [v/required [v/min-count 10]])))
 
 (defn about-page []
-  (layout/render "about.html"))
+      (layout/render "about.html"))
 
 (defn save-message! [{:keys [params]}]
-  (if-let [errors (validate-message params)]
-    (-> (response/found "/")
-        (assoc :flash (assoc params :errors errors)))
-    (do
-      (db/save-message!
-        (assoc params :timestamp (java.util.Date.)))
-      (response/found "/"))))
+      (if-let [errors (validate-message params)]
+              (-> {:errors errors} response (status 400))
+              (do
+                (db/save-message!
+                  (assoc params :timestamp (java.util.Date.)))
+                (response {:status :ok}))))
 
 (defroutes home-routes
-           (GET "/" request (home-page request))
-           (POST "/message" request (save-message! request))
+           (GET "/" request (home-page))
+           (GET "/messages" [] (response (db/get-messages)))
+           (POST "/add-message" request (save-message! request))
            (GET "/about" [] (about-page)))
