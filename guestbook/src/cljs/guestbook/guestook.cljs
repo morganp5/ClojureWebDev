04832c55b666286847bde4f1f35ce197b8ba072f
morganp5 <11314976@address.com>
2016-02-21 20:05:32 +0000

    Added wesocket configuration to clientside

--- guestbook/src/cljs/guestbook/guestook.cljs
+++ guestbook/src/cljs/guestbook/guestook.cljs
@@ -1,6 +1,7 @@
 (ns guestbook.core
   (:require [reagent.core :as reagent :refer [atom]]
-            [ajax.core :refer [GET POST]]))
+            [ajax.core :refer [GET]]
+            [guestbook.ws :as ws]))
 
 (defn get-messages
       [messages]
@@ -8,20 +9,8 @@
            {:headers {"Accept" "application/transit+json"}
             :handler #(reset! messages (vec %))}))
 
-(defn send-message! [fields errors messages]
-      (POST "/add-message"
-            {:headers {"Accept"
-                                      "application/transit+json"
-                       "x-csrf-token" (.-value (.getElementById js/document "token"))}
-             :params @fields
-             :handler #(do
-                        (reset! errors nil)
-                        (swap! messages conj (assoc @fields :timestamp (js/Date.))))
-             :error-handler #(do
-                              (.log js/console (str %))
-                              (reset! errors (get-in % [:response :errors])))}))
-
-(defn message-list [messages]
+(defn message-list
+      [messages]
       [:ul.content
        (for [{:keys [timestamp message name]} @messages]
             ^{:key timestamp}
@@ -35,35 +24,45 @@
       (when-let [error (id @errors)]
                 [:div.alert.alert-danger (clojure.string/join error)]))
 
-(defn message-form [[messages]]
-      (let [fields (atom {})
-            errors (atom nil)]
-           (fn []
-               [:div.content
-                [:div.form-group
-                 [:p "name:" (:name @fields)]
-                 [:p "message:" (:message @fields)]
-                 [errors-component errors :name]
-                 [:p "Name:"
-                  [:input.form-control
-                   {:type :text
-                    :name :name
-                    :on-change #(swap! fields assoc :name (-> % .-target .-value))
-                    :value (:name @fields)}]]]
-                [errors-component errors :message]
-                [:p "Message:"
-                 [:textarea.form-control
-                  {:rows 4
-                   :cols 50
-                   :name :message
-                   :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
-                 [:input.btn.btn-primary
-                  {:type :submit
-                   :on-click #(send-message! fields errors messages)
-                   :value "comment"}]])))
+(defn response-handler
+      [messages fields errors]
+      (fn [message]
+          (if-let [response-errors (:errors message)]
+                  (reset! errors response-errors)
+                  (do
+                    (reset! errors nil)
+                    (reset! fields nil)
+                    (swap! messages conj message)))))
+
+(defn message-form [fields errors]
+      [:div.content
+       [:div.form-group
+        [errors-component errors :name]
+        [:p "Name:"
+         [:input.form-control
+          {:type :text
+           :on-change #(swap! fields assoc :name (-> % .-target .-value))
+           :value (:name @fields)}]]
+        [errors-component errors :message]
+        [:p "Message:"
+         [:textarea.form-control
+          {:rows 4
+           :cols 50
+           :value (:message @fields)
+           :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
+        [:input.btn.btn-primary
+         {:type :submit
+          :on-click #(ws/send-message! @fields)
+          :value "comment"}]]])
 
 (defn home []
-      (let [messages (atom nil)]
+      (let [messages (atom nil)
+            errors
+            (atom nil)
+            fields
+            (atom nil)]
+           (ws/connect! (str "ws://" (.-host js/location) "/ws")
+                        (response-handler messages fields errors))
            (get-messages messages)
            (fn []
                [:div
@@ -72,7 +71,7 @@
                   [message-list messages]]]
                 [:div.row
                  [:div.span12
-                  [message-form [messages]]]]])))
+                  [message-form fields errors]]]])))
 
 (reagent/render
   [home]
