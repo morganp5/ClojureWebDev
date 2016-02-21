4ed449a668a61033ae18e4387ac988a04ae6ee10
morganp5 <11314976@mail.dcu.ie>
2016-02-21 13:00:33 +0000

    Using Selmer filters

--- html-templating/src/html_templating/core.clj
+++ html-templating/src/html_templating/core.clj
@@ -1,7 +1,39 @@
 (ns html-templating.core
-  (:require [selmer.parser :as selmer]))
+  (:require [selmer.parser :as selmer]
+            [selmer.filters :as filters]))
 
-(defn foo
-  "I don't do a whole lot."
+(defn empty-filter-test
   [x]
-  (println x "Hello, World!"))
+  "Adds a filter to check if a sequence is empty"
+  (filters/add-filter! :empty? empty?)
+  (selmer/render "{% if files|empty? %}no files{% else %}files{% endif %}"
+                 {:files []}))
+
+(defn get-filter-content
+  [x]
+  (filters/add-filter! :foo
+                       (fn [x] [:safe (.toUpperCase x)]))
+  (selmer/render "{{x|foo}}" {:y "<div>I'm safe</div>"}))
+
+(defn add-custom-tag
+  []
+  (selmer/add-tag!
+    :image
+    (fn [args context-map]
+      (str "<img src=" (first args) "/>")))
+  (selmer/render "{% image \"http://foo.com/logo.jpg\" %}" {}))
+
+(defn overload-tag
+  []
+  (selmer/add-tag!
+    :uppercase
+    (fn [args context-map content]
+      (.toUpperCase (get-in content [:uppercase :content])))
+    :enduppercase)
+  (selmer/render "{% uppercase %}foo {{bar}} baz{% enduppercase %}" {:bar "injected"}))
+
+(defn render-hello-template
+  [x]
+  (selmer/render-file "hello.html" {:name x :items (range 10)}))
+
+
