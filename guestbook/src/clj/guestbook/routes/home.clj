7789d26d212da5b36f26d46884f13957f58bf2e4
morganp5 <peter.morgan4@mail.dcu.ie>
2016-02-23 23:35:48 +0000

    Implemented Sample Restful Api

--- guestbook/src/clj/guestbook/routes/home.clj
+++ guestbook/src/clj/guestbook/routes/home.clj
@@ -1,14 +1,14 @@
 (ns guestbook.routes.home
-    (:require [guestbook.layout :as layout]
-      [guestbook.db.core :as db]
-      [compojure.core :refer [defroutes GET POST]]
-      [ring.util.response :refer [response status]]))
+  (:require [guestbook.layout :as layout]
+            [guestbook.db.core :as db]
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.util.response :refer [response status]]))
 
 (defn home-page []
-      (layout/render "home.html"))
+  (layout/render "home.html"))
 
 (defn about-page []
-      (layout/render "about.html"))
+  (layout/render "about.html"))
 
 (defroutes home-routes
            (GET "/" request (home-page))
--- guestbook/src/clj/guestbook/routes/ws.clj
+++ guestbook/src/clj/guestbook/routes/ws.clj
@@ -1,14 +1,14 @@
 (ns guestbook.routes.ws
-    (:require [compojure.core :refer [GET POST defroutes]]
-      [clojure.tools.logging :as log]
-      [mount.core :refer [defstate]]
-      [cognitect.transit :as transit]
-      [bouncer.core :as b]
-      [bouncer.validators :as v]
-      [guestbook.db.core :as db]
-      [taoensso.sente :as sente]
-      [taoensso.sente.server-adapters.immutant
-       :refer [sente-web-server-adapter]]))
+  (:require [compojure.core :refer [GET POST defroutes]]
+            [clojure.tools.logging :as log]
+            [mount.core :refer [defstate]]
+            [cognitect.transit :as transit]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [guestbook.db.core :as db]
+            [taoensso.sente :as sente]
+            [taoensso.sente.server-adapters.immutant
+             :refer [sente-web-server-adapter]]))
 
 (defonce channels (atom #{}))
 
@@ -16,47 +16,47 @@
                    sente-web-server-adapter
                    {:user-id-fn
                     (fn [ring-req] (get-in ring-req [:params :client-id]))})]
-     (def ring-ajax-post (:ajax-post-fn connection))
-     (def ring-ajax-get-or-ws-handshake (:ajax-get-or-ws-handshake-fn connection))
-     (def ch-chsk (:ch-recv connection))
-     (def chsk-send! (:send-fn connection))
-     (def connected-uids (:connected-uids connection)))
+  (def ring-ajax-post (:ajax-post-fn connection))
+  (def ring-ajax-get-or-ws-handshake (:ajax-get-or-ws-handshake-fn connection))
+  (def ch-chsk (:ch-recv connection))
+  (def chsk-send! (:send-fn connection))
+  (def connected-uids (:connected-uids connection)))
 
 (defn validate-message
-      [params]
-      (first
-        (b/validate
-          params
-          :name v/required
-          :message [v/required [v/min-count 10]])))
+  [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
 
 (defn save-message!
-      [message]
-      (if-let [errors (validate-message message)]
-              {:errors errors}
-              (do
-                (db/save-message! message)
-                message)))
+  [message]
+  (if-let [errors (validate-message message)]
+    {:errors errors}
+    (do
+      (db/save-message! message)
+      message)))
 
 (defn handle-message!
-      [{:keys [id client-id ?data]}]
-      (println "\n\n+++++++ GOT MESSAGE:" id (keys ?data))
-      (when (= id :guestbook/add-message)
-            (let [response (-> ?data
-                               (assoc :timestamp (java.util.Date.))
-                               save-message!)]
-                 (if (:errors response)
-                   (chsk-send! client-id [:guestbook/error response])
-                   (doseq [uid (:any @connected-uids)]
-                          (chsk-send! uid [:guestbook/add-message response]))))))
+  [{:keys [id client-id ?data]}]
+  (println "\n\n+++++++ GOT MESSAGE:" id (keys ?data))
+  (when (= id :guestbook/add-message)
+    (let [response (-> ?data
+                       (assoc :timestamp (java.util.Date.))
+                       save-message!)]
+      (if (:errors response)
+        (chsk-send! client-id [:guestbook/error response])
+        (doseq [uid (:any @connected-uids)]
+          (chsk-send! uid [:guestbook/add-message response]))))))
 
 (defn stop-router!
-      [stop-fn]
-      (when stop-fn (stop-fn)))
+  [stop-fn]
+  (when stop-fn (stop-fn)))
 
 (defn start-router! []
-      (println "\n\n+++++++ STARTING ROUTER! +++++++\n\n")
-      (sente/start-chsk-router! ch-chsk handle-message!))
+  (println "\n\n+++++++ STARTING ROUTER! +++++++\n\n")
+  (sente/start-chsk-router! ch-chsk handle-message!))
 
 (defstate router
           :start (start-router!)
